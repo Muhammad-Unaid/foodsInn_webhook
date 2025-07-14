219fb2dad2d24fc369ad9c35fdaeea3519fea1d1
Muhammad Unaid <unaidsaab@gmail.com>
2025-07-14 17:43:38 +0500

    working

---
+++ foodorder/orders/menu_data.py
@@ -0,0 +1,184 @@
+price_data = {
+  "OnlineDeals": [
+    { "title": "Online Deal 01", "price": 499 },
+    { "title": "Online Deal 02", "price": 860 },
+    { "title": "Online Deal 03", "price": 840 },
+    { "title": "Online Deal 04", "price": 695 },
+    { "title": "Online Deal 05", "price": 595 },
+    { "title": "Online Deal 06", "price": 1240 },
+    { "title": "Online Deal 07", "price": 875 },
+    { "title": "Online Deal 08", "price": 895 },
+  ],
+  "Starter": [
+    { "title": "Chicken Corn Soup", "price": 421 },
+    { "title": "Cream of Mushroom Soup", "price": 591 },
+    { "title": "Hot & Sour Soup", "price": 506 },
+    { "title": "Crunchy Finger Fish", "price": 1016 },
+    { "title": "Prawn Tempura", "price": 876 },
+    { "title": "Dynamite Prawns", "price": 796 },
+    { "title": "Masala Fries", "price": 265 },
+    { "title": "Spicy Juicy Wings", "price": 676 },
+    { "title": "Pizza Fries", "price": 660 },
+    { "title": "Stuffed Finger Chicken", "price": 1016 },
+    { "title": "Stuffed Chicken Kabab", "price": 846 },
+  ],
+ "Burgers": [
+    { "title": "Classic Chicken Burger", "price": 442 },
+    { "title": "Classic Beef Burger", "price": 490 },
+    { "title": "Crispy Zinger Burger", "price": 436 },
+    { "title": "BBQ Chicken Burger", "price": 434 },
+    { "title": "Jalapeno Burger", "price": 621 },
+    { "title": "Chicken Royal Burger", "price": 719 },
+    { "title": "Texas Beef Burger", "price": 591 },
+    { "title": "American Grilled Burger", "price": 655 },
+    { "title": "Mushroomella Beef Burger", "price": 625 },
+    { "title": "Beef Cheese Double Patty Burger", "price": 1075 }
+  ],
+  "Sandwiches": [
+    { "title": "Club Sandwich", "price": 931 },
+    { "title": "American Grilled Sandwich", "price": 931 },
+    { "title": "BBQ Club Sandwich", "price": 876 },
+    { "title": "Malai Club Sandwich", "price": 746 },
+    { "title": "Fajita Sandwich", "price": 716 }
+  ],
+  "Steaks": [
+    { "title": "Chicken Steak", "price": 1485 },
+    { "title": "Beef Steak", "price": 2066 }
+  ],
+  "Italian": [
+    { "title": "Alfredo Fettuccine Pasta", "price": 1165 },
+    { "title": "Alfredo Penne Pasta", "price": 1165 },
+    { "title": "Spicy Fettuccine Pasta", "price": 1165 },
+    { "title": "Spicy Penne Pasta", "price":  1101 },
+    { "title": "Parmesan Chicken Pasta", "price": 1186 },
+    { "title": "Cheesy Fettuccine Pasta", "price": 1435 }
+  ],
+  "Chinese": [
+    { "title": "Kung Pao Chicken", "price": 556 },
+    { "title": "Chicken Schezwan", "price": 520 },
+    { "title": "Chicken Shashlik BBQ", "price": 465 },
+    { "title": "Chicken Shashlik", "price": 476 },
+    { "title": "Classic Chilli Chicken Gravy", "price": 436 },
+    { "title": "Dragon Chicken", "price": 521 },
+    { "title": "Chicken Manchurian", "price": 476 },
+    { "title": "Chicken Chilli Dry", "price": 521 },
+    { "title": "Beef Chilli Dry", "price": 715 },
+    { "title": "Crispy Beijing Beef", "price": 715 }
+  ],
+  "Noodles & Rice": [
+    { "title": "Chicken Chowmein", "price": 421 },
+    { "title": "Vegetable Chowmein", "price": 298 },
+    { "title": "Singaporean Rice", "price": 506 },
+    { "title": "Chicken Fried Rice", "price": 596 },
+    { "title": "Vegetable Fried Rice", "price": 371 }
+  ],
+  "BBQ": [
+    { "title": "Chicken Tikka", "price": 591 },
+    { "title": "Bihari Tikka", "price": 591 },
+    { "title": "Chatkhara Tikka", "price": 636 },
+    { "title": "Malai Tikka", "price": 596 },
+    { "title": "Chargha", "price": 1116 },
+    { "title": "Chicken Chops", "price": 846 },
+    { "title": "Mutton Chops", "price": 2065 },
+    { "title": "Grilled Fish", "price": 1795 },
+    { "title": "Lebanese Boti", "price": 671 },
+    { "title": "Shahi Chatak Boti", "price": 746 },
+    { "title": "Malai Boti", "price": 746 },
+    { "title": "Beef Bihari Boti", "price": 846 },
+    { "title": "Creamy Kabab", "price": 694 },
+    { "title": "Beef Dhaga Kabab", "price": 846 },
+    { "title": "Reshmi Kabab", "price": 648 },
+    { "title": "Seekh Kabab", "price": 796 }
+  ],
+  "Karahi": [
+    { "title": "Chicken Karahi", "price": 1196 },
+    { "title": "Mutton Karahi", "price": 2200 },
+    { "title": "Chicken Brown Karahi", "price": 1196 },
+    { "title": "Mutton Brown Karahi", "price": 2200 },
+    { "title": "Chicken Peshawari Karahi", "price": 1187 },
+    { "title": "Mutton Peshawari Karahi", "price": 2245 },
+    { "title": "Chicken White Karahi", "price": 1516 },
+    { "title": "Balochi Tikka Karahi", "price": 1346 },
+    { "title": "Chicken Green Karahi", "price": 1436 },
+    { "title": "Tawa Brain Masala", "price": 1116 },
+    { "title": "Mixed Katakat", "price": 1612 }
+  ],
+  "Handi": [
+    { "title": "Chicken Handi", "price": 1196 },
+    { "title": "Chicken Reshmi Handi", "price": 1596 },
+    { "title": "Chicken Makhni Handi", "price": 1516 },
+    { "title": "HunzaI Chicken Handi", "price": 1596 },
+    { "title": "Chicken BBQ Handi", "price": 1196 },
+    { "title": "Daal Makhni Handi", "price": 876 }
+  ],
+  "Platters": [
+    { "title": "Chinese Platter", "price": 5381 },
+    { "title": "Classic BBQ Platter", "price": 3996 },
+    { "title": "Desi Platter", "price": 6296 },
+    { "title": "Foods Inn Special Platter", "price": 7196 },
+    { "title": "Mini BBQ Platter", "price": 2646 },
+    { "title": "Special BBQ Platter", "price": 5596 }
+  ],
+  "Rolls": [
+    { "title": "Crispy Roll", "price": 297 },
+    { "title": "Chicken Boti Roll", "price": 288 },
+    { "title": "Chicken Cheese Roll", "price": 324 },
+    { "title": "Chicken Mayo Garlic Roll", "price": 324 },
+    { "title": "Malai Boti Roll", "price": 315 },
+    { "title": "Reshmi Kabab Roll", "price": 270 },
+    { "title": "Beef Boti Roll", "price": 333 },
+    { "title": "Seekh Kabab Roll", "price": 270 }
+  ],
+  "KiddyMeals": [
+    { "title": "Junior Chicken Burger", "price": 449 },
+    { "title": "Kids Chowmein", "price": 440 },
+    { "title": "Nuggets", "price": 449 },
+    { "title": "French Fries", "price": 265 },
+    { "title": "Cold Sandwiches", "price": 421 }
+  ],
+  "RotiParatha": [
+    { "title": "Plain Naan", "price": 42 },
+    { "title": "Chapati", "price": 32 },
+    { "title": "Roghni Naan", "price": 74 },
+    { "title": "Garlic Naan", "price": 69 },
+    { "title": "Paratha", "price": 112 }
+  ],
+  "DessertIceCream": [
+    { "title": "Chocolate Lava Cake", "price": 236 },
+    { "title": "Fudge Brownie", "price": 184 },
+    { "title": "Cake Slice", "price": 405 },
+    { "title": "Whole Cake", "price": 2350 }
+  ],
+  "MocktailsShakes": [
+    { "title": "Fresh Lime", "price": 175 },
+    { "title": "Mint Lemonade", "price": 245 },
+    { "title": "Slush Drink", "price": 225 },
+    { "title": "Vanilla Caramel Shake", "price": 506 },
+    { "title": "Oreo Frappe", "price": 580 },
+    { "title": "Nutella Brownie Shake", "price": 580 },
+    { "title": "Strawberry Shake", "price": 580 },
+    { "title": "Cold Coffee", "price": 535 },
+    { "title": "Pina Colada", "price": 506 },
+    { "title": "Blue Colada", "price": 506 }
+  ],
+  "ColdBeverages": [
+    { "title": "Mineral Water", "price": 76 },
+    { "title": "Can Drink", "price": 145 },
+    { "title": "1.5 Ltr Drink", "price": 295 }
+  ],
+  "HotBeverages": [
+    { "title": "Milk Tea", "price": 200 },
+    { "title": "Cafe Latte", "price": 336 },
+    { "title": "Green Tea", "price": 105 },
+    { "title": "Black Coffee", "price": 272 },
+    { "title": "Cappuccino", "price": 336 }
+  ],
+  "AddOns": [
+    { "title": "Salad", "price": 195 },
+    { "title": "Raita", "price": 195 },
+    { "title": "Cole Slaw", "price": 35 },
+    { "title": "Crackers", "price": 150 },
+    { "title": "Mashed Potatoes", "price": 200 },
+    { "title": "Balochi Cucumber Raita", "price": 199 }
+  ]
+}
